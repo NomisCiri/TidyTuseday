a77b836408e1d99dba78aa0bf2fbc91ee2ab385c
Simon Ciranka <simon@ciranka.de>
2022-01-10 13:23:29 +0100

    added code

---
+++ 2022-week_01/Info.R
@@ -0,0 +1,15 @@
+
+# Blackrock?
+Anlagesumme=45467144553
+
+
+Streumunition_landminen=(0.52/100)*Anlagesumme
+Atomwaffen=(0.5/100)*Anlagesumme
+ZivileWaffen=(0.23/100)*Anlagesumme
+Tabak=(0.62/100)*Anlagesumme
+UN_GloablCompactViolation=(0.82/100)*Anlagesumme
+Kohle=(0.11/100)*Anlagesumme
+Ölsand=(0.24/100)*Anlagesumme
+Bruttoinlandsprodukt_Deutschland=
+
+data.frame()
